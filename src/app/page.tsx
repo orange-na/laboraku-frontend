4ede7fb6e8b248e9a6548620eaa87c06fac0cb3a
orange-na <otuy1125abman@gmail.com>
2024-07-15 17:34:58 +0900

    ✨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <Link href="/sign-up">Sing Up</Link>
+      <Link href="/sign-in">Sing In</Link>
+    </div>
+  );
 }
